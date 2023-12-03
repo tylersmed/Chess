d5db1a6079b00e3ee97148d87c6e260d20e9eb3f
Tyler Smedley <tylersmedley17@gmail.com>
2023-12-02 18:51:19 -0600

    initial setup and image download

---
+++ ChessEngine.py
@@ -0,0 +1,17 @@
+"""
+Stores all the information about the current state of the game, is responsible for for 
+determining valid moves at the current state of the game, and keeps a move log.
+"""
+
+class GameState():
+    def __init__(self):
+        self.board = [
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+            ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
+        ]
