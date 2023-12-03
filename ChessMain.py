4391c7b607aab9059de5d6e072836603c22965cc
Tyler Smedley <tylersmedley17@gmail.com>
2023-12-03 15:55:41 -0600

    draw board and pieces

--- ChessMain.py
+++ ChessMain.py
@@ -11,7 +11,7 @@ IMAGES = {}
 
 # Initialize a global dictionary of images
 def loadImages():
-    pieces = ['wP', 'wR', 'wN', 'wB', 'wK', 'bQ', 'bP', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    pieces = ['wP', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bQ', 'bP', 'bR', 'bN', 'bB', 'bK', 'bQ']
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + '.png'), (SQ_SIZE, SQ_SIZE))
         # this accesses the directory with images and maps an image to a piece
@@ -30,9 +30,34 @@ def main():
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
-
+            drawGameState(screen, gs)
             clock.tick(MAX_FPS)
             p.display.flip()
+
+# responsible for all the graphics within a current game
+def drawGameState(screen, gs):
+    drawBoard(screen) # draw squares on the board
+    drawPieces(screen, gs.board) # draw pics on top of squares
+
+
+# draw the squares on the board
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c)%2)]
+            # if the addition of r and c mod 2 has a remainder of 0, the square should be colored light
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+# draw the pieces on the board using the current GameState.board
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+    
 
 if __name__ == "__main__":
     main()
